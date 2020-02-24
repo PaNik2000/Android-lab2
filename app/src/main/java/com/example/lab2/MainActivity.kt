1e6f83ad2cb190901ecd17f80a521e7d46ea3f24
PaNik2000 <arshakov-nikita@mail.ru>
2020-02-24 12:38:09 +0300

    Added splash screen

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lab2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/lab2/SplashActivity.kt
+++ app/src/main/java/com/example/lab2/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lab2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,17 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Thread() {
+            Thread.sleep(500)
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }.start()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        finish()
     }
 }
