a1503a25037f4c4b4fc42afadaf97f2f3580162e
PaNik2000 <arshakov-nikita@mail.ru>
2020-03-21 15:02:02 +0300

    Fixed image downloading

--- app/src/main/java/com/example/lab2/MyListAdapter.kt
+++ app/src/main/java/com/example/lab2/MyListAdapter.kt
@@ -1,26 +1,24 @@
 package com.example.lab2
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.media.Image
 import android.os.AsyncTask
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.ListAdapter
 import android.widget.TextView
-import androidx.core.view.get
 import java.net.HttpURLConnection
 import java.net.URL
 
-class MyListAdapter(context: Context, resource: Int, var objects: MutableList<Technology>) :
+class MyListAdapter(context: Context, resource: Int, val objects: MutableList<Technology>) :
     ArrayAdapter<Technology>(context, resource, objects) {
+
+    init {
+        MyAsyncTask(objects).execute()
+    }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val tech = getItem(position)
@@ -34,30 +32,29 @@ class MyListAdapter(context: Context, resource: Int, var objects: MutableList<Te
 
         val temp = tech?.bitmap
         (view.findViewById(R.id.listImage) as ImageView).setImageBitmap(Bitmap.createScaledBitmap(temp!!, 128, 128, true))
-        if (tech.default) {
-            MyAsyncTask(tech, view).execute()
-        }
 
         return view
     }
 
-    class MyAsyncTask(val tech : Technology, val view : View) : AsyncTask<Void, Void, Void>() {
+    inner class MyAsyncTask(val objects: MutableList<Technology>) : AsyncTask<Void, Void, Void>() {
 
         override fun doInBackground(vararg params: Void): Void? {
-            val conn = URL("https://raw.githubusercontent.com/wesleywerner/ancient-tech/" +
-                    "02decf875616dd9692b31658d92e64a20d99f816/src/images/tech/" + tech.image).openConnection() as HttpURLConnection
 
-            if (conn.responseCode == HttpURLConnection.HTTP_OK) {
-                val inputStream = conn.getInputStream()
-                tech.bitmap = BitmapFactory.decodeStream(inputStream)
+            for (tech in objects) {
+                val conn = URL("https://raw.githubusercontent.com/wesleywerner/ancient-tech/" +
+                        "02decf875616dd9692b31658d92e64a20d99f816/src/images/tech/" + tech.image).openConnection() as HttpURLConnection
+
+                if (conn.responseCode == HttpURLConnection.HTTP_OK) {
+                    val inputStream = conn.getInputStream()
+                    tech.bitmap = BitmapFactory.decodeStream(inputStream)
+                }
+                publishProgress()
             }
             return null
         }
 
-        override fun onPostExecute(result: Void?) {
-            super.onPostExecute(result)
-            tech.default = false
-            (view.findViewById(R.id.listImage) as ImageView).setImageBitmap(Bitmap.createScaledBitmap(tech.bitmap, 128, 128, true))
+        override fun onProgressUpdate(vararg values: Void?) {
+            notifyDataSetChanged()
         }
     }
 
